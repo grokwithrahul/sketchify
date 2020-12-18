33ad4b4a2986bfffc794007c75da7cd29b9faa45
Rahul Prabhu <rahul@grokwithrahul.com>
2020-12-18 12:51:14 +0530

    v0.0.3
    Changed API

--- sketchify/sketch.py
+++ sketchify/sketch.py
@@ -10,16 +10,18 @@ def dreamsketch(path):
     return output
 
 
-def normalsketch(path, scale=10):
+def normalsketch(impath, savepath, scale=10):
     if scale > 10 or scale < 1:
         raise ValueError('Errno 1: Scale must be between 1 and 10')
     if not isinstance(scale, int):
         raise TypeError('Errno 2: Scale must be an integer value')
-    original_img = cv2.imread(path)
+    if not isinstance(impath, str) or not isinstance(savepath, str):
+        raise TypeError('Errno 3: Path to image must be a string')
+    original_img = cv2.imread(impath)
     gray_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
     gray_inverse = 255-gray_img
     sigma = scale*10
     blurred_img = cv2.GaussianBlur(gray_inverse, (51, 51), sigmaX=sigma, sigmaY=sigma)
     blurred_inverse = 255 - blurred_img
     output = cv2.divide(gray_img, blurred_inverse, scale=256.0)
-    return output
+    cv2.imwrite(savepath+'.png', output)
--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 from sketchify import sketch
 import cv2
 
-path = './tests/test_images/original/image3.jpg'
+path = './tests/test_images/original/image3.png'
 image = sketch.normalsketch(path, 1)
-cv2.imwrite('./tests/test_images/sketchified/image3.png', a)
+cv2.imwrite('./tests/test_images/sketchified/image3.png', image)
